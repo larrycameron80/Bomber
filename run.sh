edf702c4468423a10504f528cb2e91b447007742
PrivateHenker <100619098+PrivateHenker@users.noreply.github.com>
2022-02-28 21:02:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,13 +10,27 @@ echo "Ripper file: $RESOURCES_FILE_RIPPER with $INSTANCE_PER_RIPPER instances pe
 
 echo "Killing all running docker instances..."
 sudo docker rm -f $(sudo docker ps -aq) >/dev/null 2>&1 || true
-sleep 5s
+for i in {1..5} ; do
+    echo -n '['
+    for ((j=0; j<i; j++)) ; do echo -n ' '; done
+    echo -n '=>'
+    for ((j=i; j<5; j++)) ; do echo -n ' '; done
+    echo -n "] $i"0% $'\r'
+    sleep 1
+done
 echo "Killed all instances."
 
 
 echo "Starting VPN (from docker-compose)..."
 sudo docker-compose up -d
-sleep 15s
+for i in {1..15} ; do
+    echo -n '['
+    for ((j=0; j<i; j++)) ; do echo -n ' '; done
+    echo -n '=>'
+    for ((j=i; j<15; j++)) ; do echo -n ' '; done
+    echo -n "] $i"0% $'\r'
+    sleep 1
+done
 echo "VPN started."
 
 
@@ -43,7 +57,7 @@ do
    for (( c=1; c<=$INSTANCE_PER_RIPPER; c++ ))
    do
       {
-         sudo docker run -d -m 128m --cpus=2 --rm --net=container:ddos_vpn_1 nitupkcuf/ddos-ripper $RIP
+         sudo docker run -d -m 256m --cpus=2 --rm --net=container:ddos_vpn_1 nitupkcuf/ddos-ripper $RIP
       } &> /dev/null
    done
 done
