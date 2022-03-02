d6147e0cea058019009bf4f9fea621b0063d406b
PrivateHenker <100619098+PrivateHenker@users.noreply.github.com>
2022-03-02 10:41:16 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -82,7 +82,26 @@ while true; do
     echo -n "] $(expr $STEPS \* $i) / $TTL s" $'\r'
     sleep $STEPS
   done
+  LOOP_COUNT=$(expr $LOOP_COUNT + 1)
   echo "Loop ended."
   
   
+  if [ $(expr $LOOP_COUNT % 3) -eq $0 ]; then
+    echo "Taking a break for $TTL seconds."
+    sudo bash vpn-down.sh
+    sleep 5
+    for i in {1..29}; do
+      echo -n '['
+      for ((j = 0; j < i; j++)); do echo -n '#'; done
+      echo -n ''
+      for ((j = i; j < 30; j++)); do echo -n ' '; done
+      echo -n "] $(expr $STEPS \* $i) / $TTL s" $'\r'
+      sleep $STEPS
+    done
+    echo "Break is over. Restart VPN's."
+    sudo bash vpn-up.sh
+    sleep 15
+  fi
+
+
 done
